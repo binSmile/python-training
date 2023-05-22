0ceefe9db99f2e6ed2ebeb20433378dece3234c9
binSmile <36168419+binSmile@users.noreply.github.com>
2023-05-22 14:45:17 +0400

    T1L3D update

--- Yandex algo-train/Train 1/T1 Lesson 3/T1 Lesson 3 Problem D. Words count.py
+++ Yandex algo-train/Train 1/T1 Lesson 3/T1 Lesson 3 Problem D. Words count.py
@@ -1,26 +1,24 @@
 # Link to task
 # https://contest.yandex.ru/contest/27663/problems/D/
 
-# Algorithm
-#
-# Asymptotic
+# Updated solution. .read() is better then readline()
 
-def solution(lines):
-    return set(' '.join(lines).split()).__len__()
+def solution(text):
+    return set(text.split()).__len__()
 
 
-test_input = ['She sells sea shells on the sea shore;',
-"""The shells that she sells are sea shells I'm sure.""",
-"""So if she sells sea shells on the sea shore,""",
-"""I'm sure that the shells are sea shore shells."""]
+test_input = '''She sells sea shells on the sea shore;
+The shells that she sells are sea shells I'm sure.
+So if she sells sea shells on the sea shore,
+I'm sure that the shells are sea shore shells.'''
 
 assert solution(test_input) == 19
 
 
 def main():
     import sys
-    lines = sys.stdin.readlines()
-    print(solution(lines))
+    text = sys.stdin.read()
+    print(solution(text))
 
 
 if __name__ == '__main__':
